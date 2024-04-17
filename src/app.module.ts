2ac167bb70f631401a392a9c1c3febbc4c10c2ec
Kan <secureis@gmail.com>
2024-04-17 21:46:23 +0900

    module, controller, service 각 용도 확인및 기본 구성 체크

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,3 +8,4 @@ import { AppService } from './app.service';
   providers: [AppService],
 })
 export class AppModule {}
+// Nest.js에서는 하나의 모듈만 있을수 있는데, 그게 AppModule 이며, 이 모듈에서 구현하는 모든걸 import 해야 한다
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Nest!';
   }
+
+  getHi(): string {
+    return 'Hi Nest!!';
+  }
+  // 비지니스 로직은 이곳에서 처리하도록 한다.
 }
